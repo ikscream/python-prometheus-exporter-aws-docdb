f806ef50b2155e99a48dcf9f7efc1c24e3f47826
ivanov.konstantin <ivanov.konstantin.89@gmail.com>
2021-09-27 08:53:44 +0300

    Fixed TLS options

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ import pymongo
 import logging
 import os
 import gc
+from datetime import date, datetime
 
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
  
+DOCDB_TLS=os.environ['DOCDB_TLS']
+DOCDB_HOST=os.environ['DOCDB_HOST']
+DOCDB_PASSWORD=os.environ['DOCDB_PASSWORD']
+PORT=os.environ['PORT']
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
+CONFIG_CONNECTION=""
  
 docdb_connection_current = Gauge(
     'docdb_connection_current',
@@ -27,15 +33,10 @@ docdb_connection_totalCreated = Gauge(
     'Document DB connections totalCreated'
 )
 
+
 @app.route('/metrics', methods=['GET'])
 def get_data():
-
-    password = os.environ['DOCDB_PASSWORD']
-    host = os.environ['DOCDB_HOST']
-    options = ""
-    if os.environ['DOCDB_TLS'] == 1:
-        options = "/?tls=true&tlsCAFile=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
-    client = pymongo.MongoClient(f"mongodb://root:{password}@{host}:27017{options}") 
+    client = pymongo.MongoClient(CONFIG_CONNECTION) 
     
     db = client.sample_databasevim 
 
@@ -50,4 +51,17 @@ def get_data():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
+
+    # Initial debug messages
+    print(f"{datetime.now()} DOCDB_TLS: {DOCDB_TLS}")
+    print(f"{datetime.now()} DOCDB_HOST: {DOCDB_HOST}")
+    print(f"{datetime.now()} PORT: {PORT}")
+    
+    # Calculate connection string
+    options = ""
+    if DOCDB_TLS == "1":
+        options = "/?tls=true&tlsCAFile=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    CONFIG_CONNECTION=f"mongodb://root:{DOCDB_PASSWORD}@{DOCDB_HOST}:27017{options}"
+
+    # Run application
     app.run(debug=False, host='0.0.0.0', port=os.environ['PORT'])
